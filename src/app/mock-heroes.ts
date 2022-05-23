6d4730bbdaa29bc3a544ad8685cefe9a41245f28
saad-janjua <shmookhsaad9@gmail.com>
2022-05-23 09:50:47 +0500

    tour of heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,29 @@
+import { Hero } from "./hero";
+export const HEROES : Hero[] =[
+    {
+        id:12,
+        name: "Asghar Ali Ghori"
+    },
+    {
+        id:13,
+        name: "Wasif bahi"
+    },
+    {
+        id:14,
+        name: "shahnawaz bahi"
+    },
+    {
+        id:15,
+        name: "usman bahi"
+    },
+    {
+        id:16,
+        name: "zeeshan bahi"
+    },
+    { id: 18, name: 'Dr. IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado'}
+
+]
+
+
